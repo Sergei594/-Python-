f5b967e33c4b1daa36e21a2d14f8a2c92e915e6e
Sergei <serge121y@rambler.ru>
2024-05-28 20:48:31 +0300

    3 комит

--- Задание 1.py
+++ Задание 1.py
@@ -38,3 +38,75 @@ def delete_note_by_id(notes, note_id):
     else:
         print('Заметка с таким ID не найдена')
 
+def delete_note_by_title(notes):
+    print("Список заметок:")
+    for note_id, note_info in notes.items():
+        print(f'ID: {note_id}, Заголовок: {note_info["title"]}')
+    title_to_delete = input('Введите заголовок заметки для удаления: ')
+    note_ids_to_delete = [note_id for note_id, note_info in notes.items() if note_info["title"] == title_to_delete]
+    if note_ids_to_delete:
+        for note_id in note_ids_to_delete:
+            delete_note_by_id(notes, note_id)
+    else:
+        print('Заметка с таким заголовком не найдена')
+
+def delete_note(notes):
+    print("Выберите как вы хотите удалить заметку:")
+    print("1. По ID")
+    print("2. По заголовку")
+    choice = input("Ваш выбор: ")
+    if choice == '1':
+        note_id = input('Введите ID заметки для удаления: ')
+        if note_id.isdigit():
+            note_id = int(note_id)
+            delete_note_by_id(notes, note_id)
+        else:
+            print('Некорректный ввод ID заметки')
+    elif choice == '2':
+        delete_note_by_title(notes)
+    else:
+        print('Некорректный выбор, попробуйте снова')
+
+def edit_note_by_title(notes):
+    print("Список заметок:")
+    for note_id, note_info in notes.items():
+        print(f'ID: {note_id}, Заголовок: {note_info["title"]}')
+    title_to_edit = input('Введите заголовок заметки для редактирования: ')
+    note_ids_to_edit = [note_id for note_id, note_info in notes.items() if note_info["title"] == title_to_edit]
+    if note_ids_to_edit:
+        for note_id in note_ids_to_edit:
+            title = input('Введите новый заголовок заметки: ')
+            body = input('Введите новый текст заметки: ')
+            edit_note(notes, note_id, title, body)
+    else:
+        print('Заметка с таким заголовком не найдена')
+
+def list_notes(notes):
+    for note_id, note_info in notes.items():
+        print(f'ID: {note_id}, Заголовок: {note_info["title"]}')
+
+notes = load_notes()
+
+while True:
+    print('\n1. Посмотреть список заметок')
+    print('2. Добавить заметку')
+    print('3. Редактировать заметку')
+    print('4. Удалить заметку')
+    print('5. Выйти')
+    
+    choice = input('Выберите действие: ')
+    
+    if choice == '1':
+        list_notes(notes)
+    elif choice == '2':
+        title = input('Введите заголовок заметки: ')
+        body = input('Введите текст заметки: ')
+        add_note(notes, title, body)
+    elif choice == '3':
+        edit_note_by_title(notes)
+    elif choice == '4':
+        delete_note(notes)
+    elif choice == '5':
+        break
+    else:
+        print('Некорректный выбор, попробуйте снова')
