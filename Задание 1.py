e9cfbad014c374db43493c6186d7bc86d8d56ef5
Sergei <serge121y@rambler.ru>
2024-05-28 20:46:56 +0300

    2 комит

--- Задание 1.py
+++ Задание 1.py
@@ -11,3 +11,30 @@ def load_notes():
             json.dump({}, file)
         return {}
 
+def save_notes(notes):
+    file_path = os.path.join(os.getcwd(), 'notes.json')  
+    with open(file_path, 'w') as file:
+        json.dump(notes, file, indent=4)
+
+def add_note(notes, title, body):
+    note_id = len(notes) + 1
+    notes[note_id] = {'title': title, 'body': body}
+    save_notes(notes)
+    print('Заметка добавлена')
+
+def edit_note(notes, note_id, title, body):
+    if note_id in notes:
+        notes[note_id] = {'title': title, 'body': body}
+        save_notes(notes)
+        print('Заметка отредактирована')
+    else:
+        print('Заметка с таким ID не найдена')
+
+def delete_note_by_id(notes, note_id):
+    if note_id in notes:
+        del notes[note_id]
+        save_notes(notes)
+        print('Заметка удалена')
+    else:
+        print('Заметка с таким ID не найдена')
+
