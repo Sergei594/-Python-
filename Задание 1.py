52494af903c89fcfcbf10d0fa42229e2fdf7ed2a
Sergei <serge121y@rambler.ru>
2024-05-28 20:45:03 +0300

    1 комит

--- Задание 1.py
+++ Задание 1.py
@@ -1,1 +1,13 @@
-123
+import json
+import os
+
+def load_notes():
+    file_path = os.path.join(os.getcwd(), 'notes.json')  
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            return json.load(file)
+    else:
+        with open(file_path, 'w') as file:
+            json.dump({}, file)
+        return {}
+
